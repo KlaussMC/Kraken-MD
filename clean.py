18b1019c43d83395cf0c7f857c29dcde4cb67a2b
Jacob <jakieschneider13@gmail.com>
2018-05-10 09:48:29 +1000

    Git asked me to delete clean.py

--- clean.py
+++ clean.py
@@ -1,3 +1,4 @@
+import re
 def clean (code):
-    return code
-    #cleanup work to be done, such as removing escape chars and formatting in code and incomplete tags
+    return code.replace("\n", '').replace("\t", '')
+    #cleanup work to be done, such as removing lone escape chars and formatting in code and incomplete tags
